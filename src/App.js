20becebb2dde0670b6697e7fe0b46c70aed17045
SudharsonKR <sudharsonkr@gmail.com>
2023-10-04 16:13:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const data=[{
+    image : "https://thumbs.dreamstime.com/b/cute-man-face-cart…-vector-illustration-graphic-design-135024353.jpg",
+    name : "Sudharson",
+    Batch : "B43E"
+   },
+   {image : "https://thumbs.dreamstime.com/b/cute-man-face-cart…-vector-illustration-graphic-design-135024353.jpg",
+   name : "Raju",
+   Batch : "B43E"},
+   {image : "https://thumbs.dreamstime.com/b/cute-man-face-cart…-vector-illustration-graphic-design-135024353.jpg",
+   name : "Kumar",
+   Batch : "B43E"}]
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     {data.map((indexobj, idxvalue)=>(
+     <Student
+     key = {idxvalue}
+     image = {indexobj.image}
+     name = {indexobj.name}
+     Batch = {indexobj.Batch}
+      />
+     ))}
+     </div>
+    
   );
 }
 
 export default App;
+
+function Student(props)
+{
+  
+return (
+  <div className='homecomp'>
+    <img src={props.image} alt={props.name}/>
+    <h1>{props.name}</h1>
+    <h3>{props.Batch}</h3>
+   </div>
+)
+}
+
